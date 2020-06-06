46e7dc09100d7394fea457112f76eb58c8d2366d
emanuele <emanuele.sabbadini@gmail.com>
2020-06-06 08:19:40 +0200

    weather app completed - added converters

---
+++ WeatherApp/ViewModel/Converters/BoolToRainConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WeatherApp.ViewModel.Converters
+{
+    public class BoolToRainConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool rain = (bool)value;
+
+            return rain ? "Raining" : "Not raining";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
