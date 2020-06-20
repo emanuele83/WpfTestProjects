cc790cb6a08b103be7c4daacfb6775d256a77996
emanuele <emanuele.sabbadini@gmail.com>
2020-06-20 12:42:34 +0200

    added rss reader app

---
+++ RssReader/Model/Rss.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RssReader.Model
+{
+    public class CData
+    {
+        public string ActualString { get; set; }
+    }
+
+    public class Item
+    {
+        public CData Title { get; set; }
+        public CData Description { get; set; }
+        public string Link { get; set; }
+        private string pubDate;
+        public string PubDate
+        {
+            get { return pubDate; }
+            set
+            {
+                pubDate = value;
+                PublishedDate = DateTime.ParseExact(pubDate, "ddd, dd MMM yyyy HH:mm:ss GMT", CultureInfo.InvariantCulture);
+            }
+        }
+
+        public DateTime PublishedDate { get; set; }
+        public string Creator { get; set; }
+    }
+
+    public class Channel
+    {
+        public List<Item> Items { get; set; }
+
+        public string Link { get; set; }
+    }
+
+    public class FinZenBlog
+    {
+        public Channel Channel { get; set; }
+    }
+}
---
+++ RssReader/ViewModel/RssHelper.cs
@@ -0,0 +1,45 @@
+﻿using RssReader.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace RssReader.ViewModel
+{
+
+    public class RssHelper
+    {
+        public static async Task<List<Item>> GetPostsAsync()
+        {
+            List<Item> posts = new List<Item>();
+
+            XmlSerializer serializer = new XmlSerializer(typeof(FinZenBlog));
+            using(HttpClient client = new HttpClient())
+            {
+                var feed = await client.GetAsync("https://www.finzen.mx/blog-feed.xml");
+                FinZenBlog blog = (FinZenBlog)serializer.Deserialize(await feed.Content.ReadAsStreamAsync());
+
+                posts = blog.Channel.Items;
+            }
+
+            return posts;
+        }
+
+        // method to copy web resource to local file
+        public static async void DownloadToFileAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync("uri to download");
+                using (var fs = new FileStream("path to file", FileMode.CreateNew))
+                {
+                    await response.Content.CopyToAsync(fs);
+                }
+            }
+        }
+    }
+}
